527e8799fd03dfdae42915f826e35b810378742f
tejus <tejussharma5252@gmail.com>
2025-04-27 20:32:42 +0530

    added test file

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,9 +20,10 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html'],['github']],
+ 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -30,6 +31,9 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot:'only-on-failure',
+    video:'retain-on-failure',
+    headless:true
   },
 
   /* Configure projects for major browsers */
@@ -44,10 +48,10 @@ export default defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
---
+++ tests/test.spec.ts
@@ -0,0 +1,13 @@
+import { test,expect } from '@playwright/test';
+
+test('Test 1' , async({page})=>{
+await page.goto("https://saucedemo.com");
+console.log("Reached the Page");
+await expect(page).toHaveTitle('Swag Labs');
+})
+
+test('Test 2' , async({page})=>{
+    await page.goto("https://saucedemo.com");
+    console.log("Reached the Page - 2");
+    await expect(page).toHaveTitle('Swag Lab');
+    })
